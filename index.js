c09f9b46e5b195346fd7719431f8bbeafb4d72ab
sandeep-v1404 <valpasanisandeep@gmail.com>
2021-12-04 01:18:28 +0530

    feat: Connecting two containers with network

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
+
+mongoose
+    .connect("mongodb://admin:password@mongo:27017/?authSource=admin")
+    .then(() => console.log("Successfully connected to DB"))
+    .catch((e) => console.error(e));
 
 app.get("/", (req, res) => {
     res.send("Hello Sandeep");
