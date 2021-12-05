2fa08dcbae3fa7ff5e8dc5ccf8311c8c2a9bd509
sandeep-v1404 <valpasanisandeep@gmail.com>
2021-12-05 13:59:38 +0530

    feat: User Route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_PORT, MONGO_IP } = require("./config/config");
 const app = express()
 const postRoute = require("./routes/postRoute")
+const userRoute = require("./routes/userRoute")
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/posts", postRoute);
+app.use("/api/users", userRoute);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on PORT ${port}`));
